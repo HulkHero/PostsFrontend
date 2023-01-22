e71a56c62ba04d958885e6a00ffaca8201e7235e
Hammad Zafar <2019ee328@student.uet.edu.pk>
2023-01-22 23:34:29 +0500

    improved show friends

--- src/components/frienditem.jsx
+++ src/components/frienditem.jsx
@@ -3,22 +3,22 @@ import React,{useState,useEffect} from 'react'
 import Axios from "axios"
 const FriendItem = (props) => {
      const [show, setShow] = useState(false);
-     const [text, setText] = useState("");
-     console.log("props",props.id)
-     useEffect(()=>{
-        if(show){
-            console.log("inside useEFFEct friend ")
-            Axios.get(`https://nice-plum-panda-tam.cyclic.app/getStatus/${props.id}/${props.props._id}`).then((response)=>{
+    //  const [text, setText] = useState("");
+    //  console.log("props",props.id)
+    //  useEffect(()=>{
+    //     if(show){
+    //         console.log("inside useEFFEct friend ")
+    //         Axios.get(`https://nice-plum-panda-tam.cyclic.app/getStatus/${props.id}/${props.props._id}`).then((response)=>{
                 
-                console.log(response)
-                setText(response.data[0].Status)
+    //             console.log(response)
+    //             setText(response.data[0].Status)
               
-            }).catch((error)=>{
-                console.log(error)
-            })
+    //         }).catch((error)=>{
+    //             console.log(error)
+    //         })
             
-        }
-     },[show])
+    //     }
+    //  },[show])
 
   return (
     <>
@@ -31,14 +31,14 @@ const FriendItem = (props) => {
       </ListItemAvatar>
       <div >
       <ListItemText onMouseOver={()=>setShow(true)} onMouseOut={()=>setShow(false)}
-        primary={props.props.name}
+        primary={props.props.createrId.name}
         sx={{":hover":{
             cursor:"pointer",
             textDecoration:"underline"
         }}}
       >
       </ListItemText>
-       {show? <div style={{position:"absolute",minWidth:"150px",zIndex:"10000",backgroundColor:"white",marginLeft:"10px",padding:"10px",borderRadius:"10px",boxShadow:"0px 0px 10px 1px #0097a7"}} >Status: {text}</div>
+       {show? <div style={{position:"absolute",minWidth:"150px",zIndex:"10000",backgroundColor:"white",marginLeft:"10px",padding:"10px",borderRadius:"10px",boxShadow:"0px 0px 10px 1px #0097a7"}} >Status: {props.props.Status}</div>
        :" "
   }
   </div>
--- src/components/friends.jsx
+++ src/components/friends.jsx
@@ -16,16 +16,20 @@ export default function AlignItemsList() {
 
   const a = useContext(NoteContext)
     const [data, setData] = useState([]);
-    const [avatar, setAvatar] = useState();
+    
     useEffect(() => {
       if(a.id){
-    Axios.get(`https://nice-plum-panda-tam.cyclic.app/showFriends/${a.id}`).then((res) => {
-      console.log(res,"hellllll");
-      setAvatar(res.data.img)
-      console.log(res.data.img,"her")
-     setData(res.data.user.friends);
-    }) } 
-    
+    // Axios.get(`https://nice-plum-panda-tam.cyclic.app/showFriends/${a.id}`).then((res) => {
+    //   console.log(res,"hellllll");
+    //   setAvatar(res.data.img)
+    //   console.log(res.data.img,"her")
+    //  setData(res.data.user.friends);
+    // }) } 
+    Axios.get( `https://nice-plum-panda-tam.cyclic.app/myFriends/${a.id}`).then((res) => {
+      console.log("resFriends",res.data)
+      setData(res.data)}
+      )
+      }
     }, [])
     
 
@@ -41,16 +45,16 @@ export default function AlignItemsList() {
     </ListItem>
     <Divider variant='middle '></Divider>
     {data.length>0 ? data.map((element,index)=>{
-          let img12= avatar[index]
-          console.log(img12,"img12")
-          const base64= btoa(new Uint8Array(img12.avatar.data.data).reduce(function (data, byte) {
+          // let img12= avatar[index]
+          // console.log(img12,"img12")
+          const base64= btoa(new Uint8Array(element.avatar.data.data).reduce(function (data, byte) {
             return data + String.fromCharCode(byte);
         }, ''));
          
         const img=`data:image/png;base64,${base64}`
      return(
       <>
-       <FriendItem props={element} img={img} id={a.id} ></FriendItem>
+       <FriendItem  props={element} img={img} id={a.id} ></FriendItem>
      </>)
     })
     :<Typography sx={{ml:"20px"}}>No Friends to show</Typography>
